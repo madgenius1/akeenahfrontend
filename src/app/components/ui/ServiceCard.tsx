70764e26c7d619e8685db95df1069faded750de3
MadGenius <nickjuma@outlook.com>
2025-09-08 23:54:25 +0300

    Created Service Card

---
+++ src/app/components/ui/ServiceCard.tsx
@@ -0,0 +1,13 @@
+
+
+
+export default function ServiceCard() {
+    return(
+        <main>
+            <div className="lg:p-8 md:p-6 p-4 flex items-center justify-center">
+                <h3 className="text-xl text-neutral-950 font-bold">Service Card Component</h3>
+            </div>
+        </main>
+    )
+
+}
