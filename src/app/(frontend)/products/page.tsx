6b2e78b5bc73e02486906f2b4733ff1980b13f94
MadGenius <nickjuma@outlook.com>
2025-09-08 03:51:59 +0300

    Modified HeroServices

---
+++ src/app/(frontend)/products/page.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Page() {
+    return (
+        <main className="bg-white min-h-screen">
+            <div className="flex items-center justify-center">
+                <h3 className="text-neutral-950 text-3xl font-semibold">
+                    Page Content
+                </h3>
+            </div>
+        </main>
+    )
+}
--- src/app/components/landing/HeroServices.tsx
+++ src/app/components/landing/HeroServices.tsx
@@ -1,13 +1,56 @@
 
 
-
+import Image from "next/image"
 
 export default function HeroServices() {
     return (
-        <main className="min-h-screen bg-base-200">
+        <main className="min-h-screen bg-base-900">
             <div className="max-w-screen-xl mx-auto">
                 <div className="lg:p-8 md:p-6 p-4">
-                    <h3 className="text-2xl font-semibold text-center text-white">Akeenah Group Services Slideshow</h3>
+                    <div className="py-8 flex items-center justify-start">
+                        <h2 className="text-2xl lg:text-4xl md:text-3xl sm:text-3xl font-semibold uppercase text-neutral-950">
+                            Services
+                        </h2>
+                    </div>
+                    <div className="flex justify-center">
+                        <div className="carousel carousel-vertical rounded-box h-96">
+                            <div className="carousel-item h-full">
+                                <Image
+                                    src="/services/stockimage.webp"
+                                    alt="service One"
+                                    width={540}
+                                    height={420} />
+                            </div>
+                            <div className="carousel-item h-full">
+                                <Image
+                                    src="/services/stockimage.webp"
+                                    alt="service One"
+                                    width={540}
+                                    height={420} />
+                            </div>
+                            <div className="carousel-item h-full">
+                                <Image
+                                    src="/services/stockimage.webp"
+                                    alt="service One"
+                                    width={540}
+                                    height={420} />
+                            </div>
+                            <div className="carousel-item h-full">
+                                <Image
+                                    src="/services/stockimage.webp"
+                                    alt="service One"
+                                    width={540}
+                                    height={420} />
+                            </div>
+                            <div className="carousel-item h-full">
+                                <Image
+                                    src="/services/stockimage.webp"
+                                    alt="service One"
+                                    width={540}
+                                    height={420} />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </main>
